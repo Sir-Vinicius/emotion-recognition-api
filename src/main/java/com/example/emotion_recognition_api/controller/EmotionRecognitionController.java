2443aa72316f87cc409bdf908138ef68fcd1f02e
Sir-Vinicius <viniciusidai@gmail.com>
2024-08-09 11:57:09 -0300

    Att landmarks to blendshapes

--- src/main/java/com/example/emotion_recognition_api/controller/EmotionRecognitionController.java
+++ src/main/java/com/example/emotion_recognition_api/controller/EmotionRecognitionController.java
@@ -1,11 +1,5 @@
 package com.example.emotion_recognition_api.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.emotion_recognition_api.service.EmotionRecognitionService;
 
+import java.util.Map;
+
 @RestController
 public class EmotionRecognitionController {
 
-    private static final Logger logger = LoggerFactory.getLogger(EmotionRecognitionController.class);
     private final EmotionRecognitionService emotionRecognitionService;
 
     @Autowired
@@ -25,24 +20,13 @@ public class EmotionRecognitionController {
         this.emotionRecognitionService = emotionRecognitionService;
     }
 
-    @PostMapping("/detectEmotionFromLandmarks")
-    public ResponseEntity<Map<String, Object>> detectEmotion(@RequestBody List<Map<String, Double>> landmarks) {
-        Map<String, Object> response = new HashMap<>();
-        
-        // Log para depuração
-        logger.info("Recebido landmarks: " + landmarks);
-        
+    @PostMapping("/detectEmotionFromBlendshapes")
+    public ResponseEntity<Map<String, ?>> predict(@RequestBody Map<String, Double> blendshapes) {
         try {
-            Map<String, Object> result = emotionRecognitionService.detectEmotionFromLandmarks(landmarks);
-            response.put("emotion", result.get("emotion"));
-            response.put("confidence", result.get("confidence"));
+            Map<String, ?> results = emotionRecognitionService.detectEmotionFromBlendshapes(blendshapes);
+            return ResponseEntity.ok(results);
         } catch (Exception e) {
-            logger.error("Erro ao processar os landmarks", e);
-            response.put("error", "Erro ao processar os landmarks");
-            return ResponseEntity.status(500).body(response);
+            return ResponseEntity.status(500).body(Map.of("error", "Erro ao processar os blendshapes: " + e.getMessage()));
         }
-
-        return ResponseEntity.ok(response);
     }
-
 }
--- src/main/java/com/example/emotion_recognition_api/service/EmotionRecognitionService.java
+++ src/main/java/com/example/emotion_recognition_api/service/EmotionRecognitionService.java
@@ -1,19 +1,16 @@
 package com.example.emotion_recognition_api.service;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.dmg.pmml.Model;
-import org.dmg.pmml.PMML;
+import org.jpmml.evaluator.Evaluator;
 import org.jpmml.evaluator.EvaluatorUtil;
-import org.jpmml.evaluator.ModelEvaluator;
-import org.jpmml.evaluator.ModelEvaluatorFactory;
-import org.jpmml.model.PMMLUtil;
+import org.jpmml.evaluator.LoadingModelEvaluatorBuilder;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -23,76 +20,46 @@ import org.xml.sax.SAXException;
 @Service
 public class EmotionRecognitionService {
 
-    private static final String ERROR_PROCESSING_IMAGE = "Erro ao processar a imagem";
-    private final ModelEvaluator<?> evaluator;
+    private final Evaluator evaluator;
 
-    public EmotionRecognitionService() throws ParserConfigurationException {
-        try {
-            this.evaluator = loadPmmlModel();
-        } catch (IOException | SAXException | JAXBException e) {
-            String errorMessage = "Falha ao carregar o modelo PMML: " + e.getMessage();
-            throw new RuntimeException(errorMessage);
-        }
+    public EmotionRecognitionService() throws IOException, ParserConfigurationException, SAXException, JAXBException {
+        this.evaluator = loadPmmlModel();
     }
 
-    private ModelEvaluator<?> loadPmmlModel() throws IOException, SAXException, JAXBException, ParserConfigurationException {
-        try (InputStream is = new ClassPathResource("emotion_recognition_model.pmml").getInputStream()) {
-            PMML pmml = PMMLUtil.unmarshal(is);
-            System.out.println("Loaded PMML model");
-            Model model = pmml.getModels().get(0);
-            System.out.println("Loaded model: " + model.getModelName());
-            ModelEvaluatorFactory modelEvaluatorFactory = ModelEvaluatorFactory.newInstance();
-            return modelEvaluatorFactory.newModelEvaluator(pmml, model);
-        }
-    }    
+    private Evaluator loadPmmlModel() throws IOException, ParserConfigurationException, SAXException, JAXBException {
+        String modelFolder = new ClassPathResource("src/main/resources/model/mlp_model_68acc.pmml").getPath();
+        Path modelPath = Paths.get(modelFolder);
+        URL url = modelPath.toFile().toURI().toURL();
+        
+        System.out.println("URL : " + url.toString());
 
-    public Map<String, Object> detectEmotionFromLandmarks(List<Map<String, Double>> landmarks) {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            Map<String, Double> input = prepareInput(landmarks);
-            System.out.println("Input for prediction: " + input); // Adicione esta linha
-            Map<String, Object> prediction = predictEmotion(input);
-            System.out.println("Prediction result: " + prediction); // Adicione esta linha
-    
-            double predictedEmotion = (Double) prediction.getOrDefault("predictedEmotion", -1.0);
-            double confidence = (Double) prediction.getOrDefault("confidence", 0.0);
-    
-            response.put("emotion", predictedEmotion);
-            response.put("confidence", confidence);
-        } catch (Exception e) {
-            response.put("error", ERROR_PROCESSING_IMAGE + ": " + e.getMessage());
-        }
-        return response;
-    }
-    
+        Evaluator evaluator = new LoadingModelEvaluatorBuilder()
+                .load(modelPath.toFile())
+                .build();
 
-    private Map<String, Double> prepareInput(List<Map<String, Double>> landmarks) {
-        Map<String, Double> input = new HashMap<>();
-        for (int i = 0; i < landmarks.size(); i++) {
-            Map<String, Double> point = landmarks.get(i);
-            input.put("x" + i, point.get("x"));
-            input.put("y" + i, point.get("y"));
-        }
-        return input;
+        // Perform self-testing
+        evaluator.verify();
+
+        System.out.println("Modelo terminou de carregar");
+        return evaluator;
     }
 
-    private Map<String, Object> predictEmotion(Map<String, Double> input) {
-        Map<String, Object> result = new HashMap<>();
+    public Map<String, ?> detectEmotionFromBlendshapes(Map<String, Double> blendshapes) {
         try {
-            System.out.println("Input for prediction: " + input); // Adicione esta linha
-            Map<String, ?> results = evaluator.evaluate(input);
+            Map<String, ?> results = predictEmotion(blendshapes);
+            
+            // Decoupling results from the JPMML-Evaluator runtime environment
             results = EvaluatorUtil.decodeAll(results);
-            System.out.println("Raw results: " + results); // Adicione esta linha
-     
-            for (Map.Entry<String, ?> entry : results.entrySet()) {
-                String key = entry.getKey();
-                Object resultValue = entry.getValue();
-                result.put(key, resultValue);
-            }
+            
+            System.out.println("Raw results: " + results);
+            return results;
         } catch (Exception e) {
             System.err.println("Erro ao prever emoção: " + e.getMessage());
-            e.printStackTrace();
+            return Map.of("error", "Erro ao processar os blendshapes: " + e.getMessage());
         }
-        return result;
     }
-}    
+
+    private Map<String, ?> predictEmotion(Map<String, Double> data) {
+        return this.evaluator.evaluate(data);
+    }
+}
